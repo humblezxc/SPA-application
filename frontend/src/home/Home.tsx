eb634b8c66511e6b742f2d015c8c46f84d73617c
humblezxc <you@example.com>
2023-01-23 16:18:55 +0100

    Created root SPA style

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,8 +1,119 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import {Typography} from "@mui/material";
+import {useState} from "react";
+
+interface Column {
+    id: 'name' | 'code' | 'year';
+    label: string;
+    minWidth?: number;
+    align?: 'right';
+    format?: (value: number) => string;
+}
+
+const columns: Column[] = [
+    { id: 'name', label: 'id', minWidth: 170 },
+    { id: 'code', label: 'Name', minWidth: 100 },
+    { id: 'year', label: 'Year', minWidth: 150},
+    // {
+    //     id: 'population',
+    //     label: 'Population',
+    //     minWidth: 170,
+    //     align: 'right',
+    //     format: (value: number) => value.toLocaleString('en-US'),
+    // },
+];
+
+interface Data {
+    name: string;
+    code: string;
+    year: number;
+}
+
+function createData(
+    name: string,
+    code: string,
+    year: number,
+): Data {
+    return { name, code, year };
+}
+
+const rows = [
+    createData('India', 'IN', 1324171354),
+    createData('China', 'CN', 1403500365),
+    createData('Italy', 'IT', 60483973),
+    createData('United States', 'US', 327167434),
+    createData('Canada', 'CA', 37602103),
+    createData('Australia', 'AU', 25475400),
+    createData('Germany', 'DE', 83019200),
+    createData('Ireland', 'IE', 4857000),
+    createData('Mexico', 'MX', 126577691),
+    createData('Japan', 'JP', 126317000),
+    createData('France', 'FR', 67022000),
+    createData('United Kingdom', 'GB', 67545757),
+    createData('Russia', 'RU', 146793744),
+    createData('Nigeria', 'NG', 200962417),
+    createData('Brazil', 'BR', 210147125),
+];
+
 export default function (){
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [data, setData] = useState<any>(null);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('https://api.example.com/endpoint');
+            const json = await response.json();
+            setData(json);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
     return(
+        <main>
+            <Box
+                sx={{
+                    bgcolor: 'background.default',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+
+                }}
+            >
+                <Box
+                    sx={{
+                        pt:8,
+                        pb: 6,
+                    }}
+                >
+                    <Typography component="h1"
+                                variant="h2"
+                                align="center"
+                                color="text.primary"
+                                gutterBottom
+                    >
+                        SPA website
+                    </Typography>
+                </Box>
         <Box
             component="form"
             sx={{
@@ -11,10 +122,56 @@ export default function (){
             noValidate
             autoComplete="off"
         >
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <TextField id="filled-basic" label="Filled" variant="filled" />
-            <TextField id="standard-basic" label="Standard" variant="standard" />
+            <TextField id="outlined-basic" label="Outlined" type={"number"} variant="outlined" />
         </Box>
+                <Paper sx={{ width: '100%' }}>
+                    <TableContainer sx={{ maxHeight: 440 }}>
+                        <Table stickyHeader aria-label="sticky table">
+                            <TableHead>
+                               {columns.map((column) => (
+                                        <TableCell
+                                            key={column.id}
+                                            align={column.align}
+                                            style={{ top: 57, minWidth: column.minWidth }}
+                                        >
+                                            {column.label}
+                                        </TableCell>
+                                    ))}
+                            </TableHead>
+                            <TableBody>
+                                {rows
+                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map((row) => {
+                                        return (
+                                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                                {columns.map((column) => {
+                                                    const value = row[column.id];
+                                                    return (
+                                                        <TableCell key={column.id} align={column.align}>
+                                                            {column.format && typeof value === 'number'
+                                                                ? column.format(value)
+                                                                : value}
+                                                        </TableCell>
+                                                    );
+                                                })}
+                                            </TableRow>
+                                        );
+                                    })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 100]}
+                        component="div"
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Paper>
+            </Box>
 
+        </main>
     );
 }
