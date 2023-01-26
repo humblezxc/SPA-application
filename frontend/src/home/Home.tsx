e2f369e171f38ac708487b3a1b6812dda2ec4818
humblezxc <you@example.com>
2023-01-26 14:47:58 +0100

    Fetched data and displayed in table

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,94 +1,28 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import {Paper} from "@mui/material";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import {Typography} from "@mui/material";
-import {useState} from "react";
-
-interface Column {
-    id: 'name' | 'code' | 'year';
-    label: string;
-    minWidth?: number;
-    align?: 'right';
-    format?: (value: number) => string;
-}
-
-const columns: Column[] = [
-    { id: 'name', label: 'id', minWidth: 170 },
-    { id: 'code', label: 'Name', minWidth: 100 },
-    { id: 'year', label: 'Year', minWidth: 150},
-    // {
-    //     id: 'population',
-    //     label: 'Population',
-    //     minWidth: 170,
-    //     align: 'right',
-    //     format: (value: number) => value.toLocaleString('en-US'),
-    // },
-];
-
-interface Data {
-    name: string;
-    code: string;
-    year: number;
-}
-
-function createData(
-    name: string,
-    code: string,
-    year: number,
-): Data {
-    return { name, code, year };
-}
-
-const rows = [
-    createData('India', 'IN', 1324171354),
-    createData('China', 'CN', 1403500365),
-    createData('Italy', 'IT', 60483973),
-    createData('United States', 'US', 327167434),
-    createData('Canada', 'CA', 37602103),
-    createData('Australia', 'AU', 25475400),
-    createData('Germany', 'DE', 83019200),
-    createData('Ireland', 'IE', 4857000),
-    createData('Mexico', 'MX', 126577691),
-    createData('Japan', 'JP', 126317000),
-    createData('France', 'FR', 67022000),
-    createData('United Kingdom', 'GB', 67545757),
-    createData('Russia', 'RU', 146793744),
-    createData('Nigeria', 'NG', 200962417),
-    createData('Brazil', 'BR', 210147125),
-];
 
 export default function (){
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    const [data, setData] = useState<any>(null);
+    const [products, setProducts] = useState([])
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch('https://api.example.com/endpoint');
-            const json = await response.json();
-            setData(json);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
+    useEffect(() => {
+        axios.get("https://reqres.in/api/products")
+            .then(res => {
+                setProducts(res.data.data);
+            })
+            .catch(err => console.log(err));
 
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-    return(
+    }, []);
+
+        return(
         <main>
             <Box
                 sx={{
@@ -113,65 +47,56 @@ export default function (){
                     >
                         SPA website
                     </Typography>
-                </Box>
-        <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <TextField id="outlined-basic" label="Outlined" type={"number"} variant="outlined" />
-        </Box>
-                <Paper sx={{ width: '100%' }}>
-                    <TableContainer sx={{ maxHeight: 440 }}>
-                        <Table stickyHeader aria-label="sticky table">
-                            <TableHead>
-                               {columns.map((column) => (
-                                        <TableCell
-                                            key={column.id}
-                                            align={column.align}
-                                            style={{ top: 57, minWidth: column.minWidth }}
-                                        >
-                                            {column.label}
-                                        </TableCell>
-                                    ))}
-                            </TableHead>
-                            <TableBody>
-                                {rows
-                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    .map((row) => {
-                                        return (
-                                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                                {columns.map((column) => {
-                                                    const value = row[column.id];
-                                                    return (
-                                                        <TableCell key={column.id} align={column.align}>
-                                                            {column.format && typeof value === 'number'
-                                                                ? column.format(value)
-                                                                : value}
-                                                        </TableCell>
-                                                    );
-                                                })}
-                                            </TableRow>
-                                        );
-                                    })}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <TablePagination
-                        rowsPerPageOptions={[5, 10, 100]}
-                        component="div"
-                        count={rows.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onPageChange={handleChangePage}
-                        onRowsPerPageChange={handleChangeRowsPerPage}
-                    />
-                </Paper>
-            </Box>
+                    <div>
+                        {products && <div>{JSON.stringify(products)}</div>}
+                    </div>
 
+                </Box>
+
+            </Box>
+            <Box
+                sx={{
+                    bgcolor: 'background.default',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+
+                }}
+            >
+                <Box
+                    sx={{
+                        pt:8,
+                        pb: 6,
+                    }}
+                >
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead >
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Year</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {products.map((product) => (
+                            <TableRow
+                                key={product["id"]}
+                                // onClick={() => handleRowClick(product)}
+                                style={{ backgroundColor: product["color"] }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {product["id"]}
+                                </TableCell>
+                                <TableCell align="right">{product["name"]}</TableCell>
+                                <TableCell align="right">{product["year"]}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+                </Box>
+            </Box>
         </main>
     );
 }
